21811e3dc3921db0cb51a8b882bd7ecc8d2c5964
Rahul Bisht <rahulbisht@Administrators-MacBook-Air.local>
2022-08-16 18:37:24 +0530

    plugin update

--- Opencart_V3.x/paytm_v3.x.ocmod/admin/language/en-gb/extension/payment/paytm.php
+++ Opencart_V3.x/paytm_v3.x.ocmod/admin/language/en-gb/extension/payment/paytm.php
@@ -7,7 +7,7 @@ $_['text_payment']							= 'Payment';
 $_['text_extension']						= 'Extension';
 $_['text_payments']							= 'Payments';
 $_['text_success']							= 'Success: You have modified paytm account details!';
-// $_['text_paytm']							= '<img src="https://raw.githubusercontent.com/paytm/Paytm_Magento_Plugin/master/paytm_logo_paymodes.svg" alt="Paytm" title="Paytm" style="border: 1px solid #EEEEEE;" />';
+$_['text_paytm']							= '<img src="https://raw.githubusercontent.com/paytm/Paytm_Magento_Plugin/master/paytm_logo_paymodes.svg" alt="Paytm" title="Paytm" style="border: 1px solid #EEEEEE;" />';
 $_['text_edit']								= 'Edit Paytm Payments';
 $_['text_opencart_version']					= 'Opencart Version';
 $_['text_curl_version']						= 'cURL Version';
@@ -19,19 +19,22 @@ $_['text_response_error']					= 'Something went wrong. Please again';
 $_['text_developer_docs'] 		            = 'Developer Docs';
 $_['text_curl_disabled']					= 'cURL is not enabled properly. Please verify.';
 $_['text_production']						= 'Production';
-$_['text_staging']							= 'Staging';
+$_['text_staging']							= 'Test/Staging';
+
+$_['text_staging_website']					= 'WEBSTAGING';
+$_['text_production_website']				= 'DEFAULT';
 
 // Button
 $_['button_fetch_status']					= "Fetch Status";
 
 // Entry
-$_['entry_merchant_id']						= 'Merchant ID';
+$_['entry_merchant_id']						= 'Test/Production MID';
 $_['entry_merchant_id_help']				= 'Enter your Merchant ID provided by Paytm';
 
-$_['entry_merchant_key']					= 'Merchant Key';
+$_['entry_merchant_key']					= 'Test/Production Secret Key';
 $_['entry_merchant_key_help']				= 'Enter your Merchant Key provided by Paytm';
 
-$_['entry_website']							= 'Website Name';
+$_['entry_website']							= 'Website (Provided by Paytm)';
 $_['entry_website_help']					= 'Enter your Website Name provded by Paytm';
 
 $_['entry_industry_type']					= 'Industry Type';
@@ -62,6 +65,7 @@ $_['entry_sort_order']						= 'Sort Order';
 $_['entry_bank_offer']						= 'Enable Bank Offer';
 $_['entry_emi_subvention']					= 'Enable EMI Subvention';
 $_['entry_dc_emi']						    = 'Enable DC EMI';
+$_['entry_invert_logo']						= 'Invert Logo';
 
 
 // Error
---
+++ Opencart_V3.x/paytm_v3.x.ocmod/system/library/paytm/PaytmConstants.php
@@ -0,0 +1,32 @@
+<?php
+
+class PaytmConstants{
+	CONST PRODUCTION_HOST						= "https://securegw.paytm.in/";
+	CONST STAGING_HOST							= "https://securegw-stage.paytm.in/";
+
+	CONST ORDER_PROCESS_URL						= "order/process";
+	CONST ORDER_STATUS_URL						= "order/status";
+	CONST INITIATE_TRANSACTION_URL				= "theia/api/v1/initiateTransaction";
+	CONST CHECKOUT_JS_URL						= "merchantpgpui/checkoutjs/merchants/MID.js";
+
+	CONST SAVE_PAYTM_RESPONSE 					= true;
+	CONST CHANNEL_ID							= "WEB";
+	CONST APPEND_TIMESTAMP						= true;
+	CONST ONLY_SUPPORTED_INR					= true;
+	CONST X_REQUEST_ID							= "PLUGIN_OPENCART_" . VERSION;
+
+	CONST MAX_RETRY_COUNT						= 3;
+	CONST CONNECT_TIMEOUT						= 10;
+	CONST TIMEOUT								= 10;
+
+	CONST LAST_UPDATED							= "20220808";
+	CONST PLUGIN_VERSION						= "2.2";
+
+	CONST CUSTOM_CALLBACK_URL					= "";
+	CONST IS_BLINK_SUPPORTED					= true;
+
+	CONST COLORED_LOGO_URL						= "https://raw.githubusercontent.com/paytm/Paytm_Magento_Plugin/master/paytm_logo_paymodes.svg";
+	CONST WHITE_LOGO_URL						= "https://raw.githubusercontent.com/paytm/Paytm_Magento_Plugin/master/paytm_logo_invert.svg";
+}
+
+?>
